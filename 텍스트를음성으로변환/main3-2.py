644d119fafdcdb23b5a6085d37dda720a9487b2e
송현우 <shw7349@naver.com>
2023-07-16 22:52:39 +0900

    #3 텍스트를 음성으로 변환

---
+++ 텍스트를음성으로변환/main3-2.py
@@ -0,0 +1,17 @@
+from gtts import gTTS
+from playsound import playsound
+# 설치 : pip install gtts
+# 설치 : pip install playsound
+
+
+# 텍스트를 입력받습니다.
+text = input('텍스트를 입력하세요: ')
+
+# 한국어로 음성을 출력하도록 설정합니다.
+tts = gTTS(text, lang='ko')
+
+# 음성을 mp3 파일로 저장합니다.
+tts.save('output.mp3')
+
+# 저장한 mp3 파일을 재생합니다.
+playsound('output.mp3')
