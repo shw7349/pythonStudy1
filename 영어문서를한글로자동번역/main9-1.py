6260b414a6e268def3e640403e945dfef66c6ed5
송현우 <shw7349@naver.com>
2023-07-31 21:33:15 +0900

    #9 영어 문서를 한글로 자동번역

---
+++ 영어문서를한글로자동번역/main9-1.py
@@ -0,0 +1,23 @@
+from googletrans import Translator
+
+# install : pip install googletrans==4.0.0-rc1
+
+# 번역할 파일 경로
+input_file_path = "python\영어문서를한글로자동번역\영어문서.txt"
+
+# 번역된 파일 저장 경로
+output_file_path = "python\영어문서를한글로자동번역\한글번역.txt"
+
+# 번역기 생성
+translator = Translator()
+
+# 파일 읽기
+with open(input_file_path, "r" , encoding="utf-8") as input_file:
+    text = input_file.read()
+    
+# 번역
+result = translator.translate(text, dest="ko")
+
+# 번역된 결과를 파일에 쓰기
+with open(output_file_path, "w", encoding="utf-8") as output_file:
+    output_file.write(result.text)
